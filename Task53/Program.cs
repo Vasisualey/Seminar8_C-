bfecacd09d22beb0bc436f08f7af2d60f5218252
Vasisualey <basilnekrasov@mail.ru>
2023-02-27 10:21:26 +0300

    начал семинар

---
+++ Task53/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 53. Задайте двумерный масив. 
+// Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+Console.Clear();
+
+Console.WriteLine("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,]array = GetArray(rows, columns,1,10);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int [m,n];
+    for (int i = 0; i<m;i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i< inArray.GetLength(0); i++)
+    {
+        for (int j=0; j< inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] NewArray(int[,] array)
+{
+    int[,] NewaArray = new int[array.GetLength(0), array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j=0; j< inArray.GetLength(1); j++)
+        {
+            if((i!=0) || (i !=array.GetLength(0)-1))
+            {
+            NewArray[i,j] = array [i,j];
+            }
+        }
+    }
+}
