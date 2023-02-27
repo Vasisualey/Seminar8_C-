122bf0d8f3d5cddef47cecc31a1110ab1b679419
*Vasisualey* <basilnekrasov@mail.ru>
2023-02-27 14:52:46 +0300

    решил 55 задачу

---
+++ Task55/Program.cs
@@ -0,0 +1,68 @@
+﻿//Задача 55. Задайте двумерный массив.
+// Напишите программуб которая заменяет строки на столбцы
+// В случаеб если это не возможно, программа должна вывести сообщение
+// для польщователя
+
+Console.Clear();
+
+Console.WriteLine("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,]array = GetArray(rows, columns,1,10);
+PrintArray(array);
+Console.WriteLine();
+
+if (rows == columns)
+{
+    array = TransposeMatrix(array);
+    PrintArray(array);
+}
+else
+{
+    Console.WriteLine("Невозможно заменить строки на столбцы!");
+    Console.WriteLine("Транспонированная матрица =>");
+    PrintArray(TransposeMatrix(array));
+}
+
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int [m,n];
+    for (int i = 0; i<m;i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i< inArray.GetLength(0); i++)
+    {
+        for (int j=0; j< inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] TransposeMatrix(int[,] array)
+{
+    int[,] TransposeMatrix = new int[array.GetLength(1), array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j=0; j< array.GetLength(1); j++)
+        {
+           TransposeMatrix[j,i] = array[i,j];
+        }
+    }
+    return TransposeMatrix;
+}
+
